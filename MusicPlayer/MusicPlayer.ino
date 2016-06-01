00b56187ec58be9e1e253ac61c834e118150c748
cannonjm <cannonjm@MSOE-CND52919BC>
2016-06-01 00:11:42 -0500

    Add project files.

---
+++ MusicPlayer/MusicPlayer.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		MusicPlayer.ino
+ Created:	6/1/2016 12:11:39 AM
+ Author:	cannonjm
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
